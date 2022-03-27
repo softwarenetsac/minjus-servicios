ebf4d975a9226fcd998daf206a101b1a37df290a
Martin Llamas Mendoza <47331862+jhonmartinllm@users.noreply.github.com>
2022-03-27 16:53:34 -0500

    carga inicial

--- README.md
+++
@@ -1,2 +0,0 @@
-# mcs-indicadores
-Servicio de reportes.
--- src/main/java/pe/gob/minsa/indicadores/dao/UsuarioDao.java
+++
@@ -1,7 +0,0 @@
-package pe.gob.minsa.indicadores.dao;
-
-public interface UsuarioDao {
-
-//	Usuario buscarUsuarioPorUsername(String username);	
-
-}
