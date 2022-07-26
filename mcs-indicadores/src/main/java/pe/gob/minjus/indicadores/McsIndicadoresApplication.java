b377f28787106f2d9cb221722228decf1490f58e
Ezechias Palacios Meza <47331862+jhonmartinllm@users.noreply.github.com>
2022-07-25 19:16:36 -0500

    Generar archivo war

--- mcs-indicadores/src/main/java/pe/gob/minjus/indicadores/McsIndicadoresApplication.java
+++ mcs-indicadores/src/main/java/pe/gob/minjus/indicadores/McsIndicadoresApplication.java
@@ -2,13 +2,14 @@ package pe.gob.minjus.indicadores;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
 public class McsIndicadoresApplication extends SpringBootServletInitializer {
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(new Class[] { McsIndicadoresApplication.class });
-	}
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(new Class[]{McsIndicadoresApplication.class});
+    }
 }
--- mcs-indicadores/src/main/java/pe/gob/minjus/indicadores/controller/ExportarController.java
+++ mcs-indicadores/src/main/java/pe/gob/minjus/indicadores/controller/ExportarController.java
@@ -10,8 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
